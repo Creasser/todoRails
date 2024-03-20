9b10f955518232a4cda47520074281bfa14de3d4
Creasser <blakecreasser@aggienetwork.com>
2024-03-19 21:00:12 -0500

    add create route and method

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,17 @@
 class TodosController < ApplicationController
+
+    def create
+        todo = Todo.create(todo_params)
+        if todo.valid?
+            render json: todo, status: :created
+        else
+            render json: {errors: todo.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def todo_params
+        params.permit(:title, :description)
+    end
 end

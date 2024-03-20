eaacd8264e6027167458465841c467d70e7906e0
Creasser <blakecreasser@aggienetwork.com>
2024-03-20 00:49:16 -0500

    open update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :todos, only: [:create, :destroy, :index]
+  resources :todos, only: [:create, :destroy, :index, :update]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
